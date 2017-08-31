ff65c19a54eddbb4c562ad38208bd62264c9fc2a
HugoVR10 <30904739+HugoVR10@users.noreply.github.com>
2017-08-30 23:01:33 -0500

    Add files via upload

---
+++ src/Práctica02/src/SNode.java
@@ -0,0 +1,20 @@
+package Práctica02.src;
+
+
+public class SNode<T> {
+	public T value;
+	public SNode<T> next;
+	public Object object;
+	
+	public SNode()
+	{
+		value = null;
+		next = null;
+	}
+	
+	public SNode(T value)
+	{
+		this.value = value;
+		next = null;
+	}
+}
